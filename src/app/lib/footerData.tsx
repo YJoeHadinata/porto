101bbbe0301cf51dee61d8568c41adccd9f92ec7
Yoshua Joe <yoshuajoe26@gmail.com>
2023-11-27 20:10:10 +0700

    update

--- src/app/lib/footerData.tsx
+++ src/app/lib/footerData.tsx
@@ -2,6 +2,6 @@ export const footerData = {
     linkedin: "https://www.linkedin.com/in/yoshua-joe-hadinata-68ba67222/",
     email: "mailto:yoshuajoehadinata@gmail.com",
     author: "Yoshua Joe Hadinata",
-    copyright: "&copy;2023",
+    copyright: "&copy; 2023",
   };
   
